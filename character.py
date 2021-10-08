f41245e56d86e29222d2c564bb28f302f95a1d49
undertheacorntree <Libbyzeb@icloud.com>
2021-10-08 14:18:18 +0100

    setting up a base

---
+++ character.py
@@ -0,0 +1,9 @@
+import pygame
+
+# class to generate our character!
+class Character(pygame.sprite.Sprite):
+
+    def __init__(self, ):
+        super().__init__()
+
+        self.image = pygame.image.load() # insert img here
--- project.py
+++ project.py
@@ -1,1 +1,29 @@
 import pygame
+from character import Character
+
+# starts pygame
+pygame.init()
+
+# required info
+bg_colour = (77,166,255)
+size = (800,600) 
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("my project")
+
+# settings for game loop
+current_game = True
+clock = pygame.time.Clock()
+
+# game loop
+while current_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            current_game = False
+
+    screen.fill(bg_colour)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+# exit
+pygame.quit()
