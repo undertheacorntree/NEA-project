62f37ff8d2fd296b3051ee25ccaa758e5622d1b5
undertheacorntree <libbyzeb@icloud.com>
2021-10-13 12:01:57 +0100

    fixing base layout

--- character.py
+++ character.py
@@ -1,9 +1,10 @@
-import pygame
+import pygame, os
 
 # class to generate our character!
 class Character(pygame.sprite.Sprite):
 
-    def __init__(self, ):
-        super().__init__()
-
-        self.image = pygame.image.load() # insert img here
+    def __init__(self, pos):
+        super(Character, self).__init__()
+        self.image = pygame.image.load(os.path.join('assets', 'blue_alien.png'))
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
--- project.py
+++ project.py
@@ -1,29 +1,37 @@
 import pygame
 from character import Character
 
+SCREEN_WIDTH = 1800
+SCREEN_HEIGHT = 1200
+
 # starts pygame
 pygame.init()
 
 # required info
 bg_colour = (77,166,255)
-size = (800,600) 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption("my project")
 
+character = Character((200,300))
+group = pygame.sprite.RenderPlain()
+group.add(character)
+
 # settings for game loop
-current_game = True
 clock = pygame.time.Clock()
+current_game = True
 
 # game loop
 while current_game:
+
+    # Check events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             current_game = False
 
     screen.fill(bg_colour)
-
+    group.draw(screen)
     pygame.display.flip()
     clock.tick(60)
 
-# exit
+# close pygame
 pygame.quit()
