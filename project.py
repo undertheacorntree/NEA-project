3c6b31e5203a35ec495402ef8b5a9d4cb2c2b648
undertheacorntree <libbyzeb@icloud.com>
2021-10-13 12:08:57 +0100

    updating comments and checking variables, etc

--- project.py
+++ project.py
@@ -1,33 +1,36 @@
 import pygame
 from character import Character
 
+# constants for resolution
 SCREEN_WIDTH = 1800
 SCREEN_HEIGHT = 1200
 
-# starts pygame
+# initiating pygame
 pygame.init()
 
-# required info
+# screen info
 bg_colour = (77,166,255)
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-pygame.display.set_caption("my project")
+pygame.display.set_caption("my project title")
 
+# sprite info
 character = Character((200,300))
 group = pygame.sprite.RenderPlain()
 group.add(character)
 
-# settings for game loop
+# settings for game loop & fps
 clock = pygame.time.Clock()
 current_game = True
 
 # game loop
 while current_game:
 
-    # Check events
+    # check events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             current_game = False
 
+    # screen control
     screen.fill(bg_colour)
     group.draw(screen)
     pygame.display.flip()
